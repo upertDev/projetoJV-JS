625295b9afcf7dda98121c34b92725e10a97b1c8
Daniel0ffice <dannysilver97@gmail.com>
2024-01-24 16:39:03 -0300

    agora vai

---
+++ script.js
@@ -0,0 +1,17 @@
+// Initial Data
+let quadro = {
+    a1: ' ', a2:' ', a3:' ',
+    b1: ' ', b2:' ', b3:' ',
+    c1: ' ', c2:' ', c3:' '
+
+};
+let player = ' ';
+let warnig = ' ';
+let playing = flase;
+
+//Events
+
+document.querySelector('.reset').addEventListener('click', reset);
+
+//Functions
+warnig = ' ';
